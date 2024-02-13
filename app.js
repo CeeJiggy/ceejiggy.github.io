639b7e231e3cb73099bd2c59d62ca1b3514657d5
Chase <chasebrandes@yahoo.com>
2024-02-13 15:17:11 -0500

    Removed bad stuff

--- app.js
+++ app.js
@@ -7,12 +7,6 @@ const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 checkboxes.forEach((checkbox) => {
     checkbox.addEventListener('change', saveCheckboxStates);
     checkbox.addEventListener('change', dateFinished);
-    // if (checkbox.nextSibling.nodeName === '#text') {
-    //     let textToIndent = checkbox.nextSibling;
-    //     console.log(textToIndent);
-    //     let indent = document.createElement('span');
-    //     textToIndent.innerHTML.append(indent);
-    // }
 });
 
 function saveCheckboxStates() {
